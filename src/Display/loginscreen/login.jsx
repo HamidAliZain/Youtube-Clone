b95535f154ea10364e09cc96acee0b43c2df2e00
HAmid <hamidprince823@gmail.com>
2022-01-19 15:57:34 +0500

    LOgun Setup

---
+++ src/Display/loginscreen/login.jsx
@@ -0,0 +1,15 @@
+import "./_login.scss"
+export const LoginScreen = () => {
+  return (
+    <div className="login">
+      <div className="login_Container">
+        <img
+          src="https://www.freepnglogos.com/uploads/youtube-logo-png-images-0.png"
+          alt=""
+        />
+        <button>Login With Google</button>
+        <p>This is made for Practice</p>
+      </div>
+    </div>
+  );
+};
